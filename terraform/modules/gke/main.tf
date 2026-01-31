1e78dd7fec5f84cce6f5862612aa763f7935581e
shashavalidarzi393-tech <shashavalidarzi393@gmail.com>
2026-01-31 20:10:20 +0530

    Update main.tf

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -2,6 +2,8 @@ resource "google_container_cluster" "gke" {
   name     = "hackathon-gke"
   location = var.region
 
+  node_locations = ["us-central1-a"]   # ✅ SINGLE ZONE
+
   remove_default_node_pool = true
   initial_node_count       = 1
   deletion_protection      = false
@@ -12,18 +14,13 @@ resource "google_container_node_pool" "primary" {
   cluster  = google_container_cluster.gke.name
   location = var.region
 
-  node_count = 1
+  node_count = 1   # ✅ ONE NODE
 
   node_config {
     machine_type    = "e2-medium"
     disk_type       = "pd-standard"
-    disk_size_gb    = 20
+    disk_size_gb    = 20     # ✅ QUOTA SAFE
     service_account = var.service_account_email
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  management {
-    auto_upgrade = true
-    auto_repair  = true
   }
 }
