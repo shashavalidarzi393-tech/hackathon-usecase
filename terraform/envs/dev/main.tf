80e35fee6dbce27b8c4435ebaaa2b892604edeaf
shashavalidarzi393-tech <shashavalidarzi393@gmail.com>
2026-01-31 17:05:01 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,9 +1,31 @@
+terraform {
+  required_version = ">= 1.5"
+
+  backend "gcs" {
+    bucket = "tf-state-hackathon-dev"
+    prefix = "terraform/state/dev"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
 module "iam" {
   source     = "../../modules/iam"
-  project = var.project_id
+  project_id = var.project_id
 }
 
 module "gke" {
-  source                 = "../../modules/gke"
-  service_account_email  = module.iam.gke_sa_email
+  source              = "../../modules/gke"
+  project_id          = var.project_id
+  region              = var.region
+  service_account_email = module.iam.gke_node_sa_email
+  network             = module.vpc.vpc_name
 }
