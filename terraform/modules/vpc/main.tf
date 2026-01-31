c32ccbc6a5ca0b86f96de404f218d72b55d4d926
shashavalidarzi393-tech <shashavalidarzi393@gmail.com>
2026-01-31 17:06:45 +0530

    Add subnet resource to VPC configuration

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,4 +1,11 @@
 resource "google_compute_network" "vpc" {
-  name = "hackathon-vpc"
+  name                    = "hackathon-vpc"
   auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "hackathon-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc.id
 }
