2a1a963155fadf2eb5426a5391b17d1bdd6cd923
shashavalidarzi393-tech <shashavalidarzi393@gmail.com>
2026-01-31 17:13:45 +0530

    Update variables.tf

--- terraform/modules/gke/variables.tf
+++ terraform/modules/gke/variables.tf
@@ -1,3 +1,15 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
 variable "service_account_email" {
+  type = string
+}
+
+variable "network" {
   type = string
 }
